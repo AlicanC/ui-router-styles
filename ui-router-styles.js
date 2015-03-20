b1190b4661df99423baae7db9e42390114f08362
Alican Çubukçuoğlu <alicancubukcuoglu@gmail.com>
2015-03-20 14:14:48 +0200

    Renamed "templateStyle" to "templateStyleUrl" and code style fixes.

--- ui-router-styles.js
+++ ui-router-styles.js
@@ -6,47 +6,70 @@ angular.module('ui.router.styles', ['ui.router'])
 		'$interpolate',
 		function ($rootScope, $state, $compile, $interpolate) {
 
-			var getStyles = function getStyles (state) {
+			var getStyle = function getStyle(templateStyleUrl) {
+
+				var style = {
+					href: templateStyleUrl,
+					rel: 'stylesheet'
+				}
+
+				if (style.href.match(/\.less$/)) {
+					style.rel = 'stylesheet/less';
+				}
+
+				return style;
+
+			};
+
+			var getStyles = function getStyles(state) {
 
 				var states = {};
 
 				//Check state for styles
-				while (state.name!=='') {
+				while (state.name !== '') {
 					var parent = $state.get('^', state);
 
 					//Initiate our view list
-					if (!states[parent.name])
+					if (!states[parent.name]) {
 						states[parent.name] = {};
+					}
 
 					//Check the templateStyle
-					if (state.templateStyle)
-						if (!states[parent.name][''])
-							states[parent.name][''] = state.templateStyle;
+					if (state.templateStyleUrl) {
+						if (!states[parent.name]['']) {
+							states[parent.name][''] = getStyle(state.templateStyleUrl);
+						}
+					}
 
 					//Check views
-					if (state.views)
+					if (state.views) {
 						_.forEach(state.views, function (view, key) {
+
 							//Check if we have a style
-							if (!view.templateStyle)
+							if (!view.templateStyleUrl) {
 								return;
+							}
 
 							//Check if we are targeting some parent
 							if ((key = key.split('@'))[1]) {
 								//Check if we have styles for that parent
-								if (!states[key[1]])
+								if (!states[key[1]]) {
 									states[key[1]] = {};
+								}
 
 								//Place the style on some parent's view
-								if (!states[key[1]][key[0]])
-									states[key[1]][key[0]] = view.templateStyle;
+								if (!states[key[1]][key[0]]) {
+									states[key[1]][key[0]] = getStyle(view.templateStyleUrl);
+								}
 
 								return;
 							}
 
 							//Place the style on our parent's view
-							states[parent.name][key[0]] = view.templateStyle;
+							states[parent.name][key[0]] = getStyle(view.templateStyleUrl);
 
 						});
+					}
 
 					//Continue with the parent
 					state = parent;
@@ -55,10 +78,15 @@ angular.module('ui.router.styles', ['ui.router'])
 				//Flatten the list
 				var flat = [];
 				_.forEach(states, function (views) {
+
 					_.forEach(views, function (style) {
-						if (!_.includes(flat, style))
+
+						if (!_.includes(flat, style)) {
 							flat.push(style);
+						}
+
 					});
+
 				});
 
 				//Reverse it so we have a proper hierarchy
@@ -74,26 +102,12 @@ angular.module('ui.router.styles', ['ui.router'])
 
 					scope.templateStyles = [];
 
-					var start = $interpolate.startSymbol();
-					var end = $interpolate.endSymbol();
+					var html = '<link ng-attr-rel="{{style.rel}}" ng-repeat="style in templateStyles" ng-href="{{style.href}}">';
 
-					var html = '<link rel="stylesheet" ng-repeat="style in templateStyles" ng-href="'+start+'style'+end+'">';
+					html = html.replace(/\{\{/g, $interpolate.startSymbol());
+					html = html.replace(/\}\}/g, $interpolate.endSymbol());
 					
 					elem.append($compile(html)(scope));
-
-					// Get the parent state
-					/*var $$parentState = function(state) {
-
-						//Check if state has a parent property
-						if (state.parent)
-
-						// Check if state has explicit parent OR we try guess parent from its name
-						var name = state.parent || (/^(.+)\.[^.]+$/.exec(state.name) || [])[1];
-
-						// If we were able to figure out parent name then get this state
-						return name && $state.get(name);
-
-					};*/
 
 					$rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
 
